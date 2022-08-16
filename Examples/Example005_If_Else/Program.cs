d5187ca65a7b1df14a18d4b084250f82a0a9e401
Irek <general157@mail.ru>
2022-08-16 21:03:25 +0300

    add Example005_If_Else

---
+++ Examples/Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Урра, это Маша!");
+}
+else
+{
+    Console.Write($"Привет {username}");
+}
